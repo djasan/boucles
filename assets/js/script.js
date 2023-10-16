2239d24017a59a12545be4db3d2bfe1dda69da3a
djamila key <sansondjamila@orange.fr>
2023-10-16 14:39:07 +0200

    sv

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+const tableau = ["entree1", "entree2", "entree3"];
+// Boucles
+// foreach
+
+tableau.forEach((element, index) => {
+  console.log(element, index);
+});
+
+// for
+
+for (let i = 0; i < tableau.length; i++) {
+  // const element = tableau[i];
+  console.log(i);
+}
+// while
+
+let n = 0;
+while (n < 10) {
+  console.log(n);
+  n++;
+
+}
